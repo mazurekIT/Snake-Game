40f3d9630c8d8c79f5123793e1ff85dff688cfee
Mateusz Mazurek <mazzi198913@wp.pl>
2019-01-24 22:03:08 +0100

    second day

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,23 +1,44 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Gameplay extends JPanel {
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
     private int[] snakeXlength = new int[750];
     private int[] snakeYlength = new int[750];
 
+    private boolean left = false;
+    private boolean right = false;
+    private boolean up = false;
+    private boolean down = false;
+
+    private ImageIcon rightmouth;
+    private ImageIcon upmouth;
+    private ImageIcon downmouth;
+    private ImageIcon leftmouth;
+
+    private Timer timer;
+    private int delay = 100;
+    private ImageIcon snakeimage;
+
+
     private ImageIcon titleImage;
 
-
     public Gameplay() {
-
-
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
     }
 
-    public void paint (Graphics g){
+    public void paint(Graphics g) {
         //draw title image border
         g.setColor(Color.WHITE);
-        g.drawRect(24,10,851,55);
+        g.drawRect(24, 10, 851, 55);
 
         //draw title image
         titleImage = new ImageIcon("assets/snaketitle.jpg");
@@ -25,10 +46,33 @@ public class Gameplay extends JPanel {
 
         // draw border for gameplay
         g.setColor(Color.WHITE);
-        g.drawRect(24, 74 ,851 , 577);
+        g.drawRect(24, 74, 851, 577);
 
         //draw bacground for bacground
         g.setColor(Color.DARK_GRAY);
         g.fillRect(25, 75, 850, 575);
+
+
+        g.dispose();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
