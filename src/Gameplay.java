5153f68049b46689b9c0b95aa4cf6b3b847a9cc4
Mateusz Mazurek <mazzi198913@wp.pl>
2019-02-03 11:41:33 +0100

    it's moving

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
@@ -25,6 +26,19 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private Timer timer;
     private int delay = 100;
     private ImageIcon snakeimage;
+
+    private int enemyxpos[]={25,50,75,100,125,150,175,200,225,250,275,300,
+    325,350,375,400,425,450,475,500,525,550,575,600,625,650,675,700,725,
+            750,775,800,825,850};
+    private int enemyypos[]={25,50,75,100,125,150,175,200,225,250,275,300,
+            325,350,375,400,425,450,475,500,525,550,575,600,625};
+
+    private ImageIcon enemyimage;
+    private Random random = new Random();
+    private int xpos = random.nextInt(34);
+    private int ypos = random.nextInt(23);
+
+
 
     private int moves = 0;
 
@@ -95,6 +109,10 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 
             }
         }
+
+        enemyimage = new ImageIcon("assets/enemy.png");
+
+
 
         g.dispose();
     }
